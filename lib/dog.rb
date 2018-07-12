e3f1866495dfc5e429f0dd11a406337d2172eef9
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-12 19:13:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dog 
+  
+  # def initialize(name)
+  #   @name = name
+  # end 
+  
+  
+  def bark 
+    puts "Woof!"
+  end 
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
